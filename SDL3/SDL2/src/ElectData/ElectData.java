073ce94584804306195d5bed67ee642aa7ca948d
HemaG13 <goskondahema13@gmail.com>
2020-06-04 12:43:41 -0400

    first commit

---
+++ SDL3/SDL2/src/ElectData/ElectData.java
@@ -0,0 +1,9 @@
+package ElectData;
+
+public class ElectData {
+    public String Election_Name;
+    public int Candidate_Count;
+    public  String date;
+    public String Start_Time;
+    public String End_Time;
+}
